8a9b25539efd24626e3538c1bab5b5839cff746d
hyeon12 <56420106+hyeon12@users.noreply.github.com>
2021-04-08 13:24:50 +0900

    .

---
+++ c++/c++/1149.cpp
@@ -0,0 +1,38 @@
+/*
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	int list[1001][3];
+	cin >> n;
+	while (n < 2 || n>1000) {
+		cout << "2<=n<=1000\n";
+		cin.clear();
+		cin.ignore(999, '\n');
+		cin >> n;
+	}
+	for (int i = 0; i < n; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		list[i][0] = a;
+		list[i][1] = b;
+		list[i][2] = c;
+	}
+
+	for (int i = 1; i < n; i++) {
+		list[i][0] += min(list[i - 1][1], list[i - 1][2]);
+		list[i][1] += min(list[i - 1][0], list[i - 1][2]);
+		list[i][2] += min(list[i - 1][1], list[i - 1][0]);
+	}
+	int an = min(list[n-1][0], list[n-1][1]);
+	an = min(an, list[n-1][2]);
+	cout << an;
+
+	return 0;
+}
+*/
